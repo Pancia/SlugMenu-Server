03c60ed1e9b8a2d7ce8eb7e5004311517988b618
Pancia <anthony.dayzerostudio@gmail.com>
2015-01-17 05:03:10 -0800

    Upgraded average to use user param so duplicates get overwritten.

--- lib/rest.ex
+++ lib/rest.ex
@@ -17,85 +17,92 @@ defmodule Slugmenu.RestServer do
     SR.create(SR, @crown)
 
     :hackney.start
-    IO.puts "Accepting rest requests on port 8080"
+    IO.puts "Accepting http requests on port 8080"
     Urna.start(Slugmenu.RestServer, port: 8080)
   end
 
-  defp add_to_avg(item, {len, avg}) do
-    new_len = len+1
-    new_avg = (avg*len/new_len) + item/new_len
-    {len+1, new_avg}
+  defp add_to_avg(rating, user, {len, avg, usr_ratings}) do
+    if Map.has_key?(usr_ratings, user)do
+      new_avg = avg + rating/len - (usr_ratings[user])/len
+      {len, new_avg, Map.put(usr_ratings, user, rating)}
+    else
+      new_len = len+1
+      new_avg = (avg*len/new_len) + rating/new_len
+      {len+1, new_avg, Map.put(usr_ratings, user, rating)}
+    end
   end
 
   defp get_rating!(bucket, food) do
     case SB.get(bucket, food) do
       {l, r} -> {l, r}
-      _ -> SB.put_and_get(bucket, food, {1, 2.5})
+      _ -> SB.put_and_get(bucket, food, {0,-1,%{}})
     end
   end
 
-  defp api_get_rating(dh, food) do
+  defp api_get_avg_rating(dh, food) do
     {:ok, bucket} = SR.lookup(SR, dh)
-    {_len, rating} = get_rating!(bucket, food)
+    {_len, rating, _usr_ratings} = get_rating!(bucket, food)
+    IO.puts "GET dh: #{dh}, food: #{food}, rating: #{rating}"
     %{status: "ok",
       rating: rating}
   end
 
-  defp api_post_rating(dh, food, rating) do
+  defp api_post_rating(dh, food, rating, user) do
     {:ok, bucket} = SR.lookup(SR, dh)
     SB.update(bucket, food, fn old ->
-      rating |> add_to_avg(old||{1,2.5}) end)
-      {_len, rating} = SB.get(bucket, food)
-      %{status: "ok",
-        rating: rating}
+      rating |> add_to_avg(user, old||{0,-1,%{}}) end)
+    {_len, rating, _usr_ratings} = SB.get(bucket, food)
+    IO.puts "POST dh: #{dh}, food: #{food}, rating: #{rating}"
+    %{status: "ok",
+      rating: rating}
   end
 
   #iex> HTTPoison.get! "localhost:8080/ratings/nine/pizza"
-  #iex> HTTPoison.post!("localhost:8080/ratings/nine/pizza", "{\"rating\": 5}", %{"Content-type" => "application/json"}).body
+  #iex> HTTPoison.post!("localhost:8080/ratings/nine/pizza", "{\"rating\": 5, \"user\": \"pancia\"}", %{"Content-type" => "application/json"}).body
   namespace :ratings do
     resource :nine do
       get food do
-        api_get_rating(@nine, food)
+        api_get_avg_rating(@nine, food)
       end
 
       post food do
-        api_post_rating(@nine, food, params["rating"])
+        api_post_rating(@nine, food, params["rating"], params["user"])
       end
     end
     resource :eight do
       get food do
-        api_get_rating(@eight, food)
+        api_get_avg_rating(@eight, food)
       end
 
       post food do
-        api_post_rating(@eight, food, params["rating"])
+        api_post_rating(@eight, food, params["rating"], params["user"])
       end
     end
     resource :cowell do
       get food do
-        api_get_rating(@cowell, food)
+        api_get_avg_rating(@cowell, food)
       end
 
       post food do
-        api_post_rating(@cowell, food, params["rating"])
+        api_post_rating(@cowell, food, params["rating"], params["user"])
       end
     end
     resource :porter do
       get food do
-        api_get_rating(@porter, food)
+        api_get_avg_rating(@porter, food)
       end
 
       post food do
-        api_post_rating(@porter, food, params["rating"])
+        api_post_rating(@porter, food, params["rating"], params["user"])
       end
     end
     resource :crown do
       get food do
-        api_get_rating(@crown, food)
+        api_get_avg_rating(@crown, food)
       end
 
       post food do
-        api_post_rating(@crown, food, params["rating"])
+        api_post_rating(@crown, food, params["rating"], params["user"])
       end
     end
   end
