e0843cea20637ef7d5bb9f64f69f96104c03141a
Anthony D'Ambrosio <nsa.anthony@gmail.com>
2015-02-23 03:16:12 -0800

    Storing parsed cmdl args in Mix Project config, getting the port in supervisor.ex

--- lib/supervisor.ex
+++ lib/supervisor.ex
@@ -12,12 +12,13 @@ defmodule Slugmenu.Supervisor do
   @server_sup_name Slugmenu.Server.TaskSupervisor
 
   def init(:ok) do
+    {port, _} = Integer.parse(Mix.Project.config()[:port])
     children = [
       worker(GenEvent, [[name: @manager_name]]),
       worker(Slugmenu.Bucket.Supervisor, [[name: @bucket_sup_name]]),
       worker(Slugmenu.Registry, [@manager_name, @bucket_sup_name, [name: @registry_name]]),
       supervisor(Task.Supervisor, [[name: @server_sup_name]]),
-      worker(Task, [@server_name, :accept, [4040]]),
+      worker(Task, [@server_name, :accept, [port]]),
       worker(Slugmenu.RestServer, [])
     ]
 
--- mix.exs
+++ mix.exs
@@ -2,10 +2,13 @@ defmodule Slugmenu.Mixfile do
   use Mix.Project
 
   def project do
+    {opts, _argv, _errors} = OptionParser.parse(System.argv(), strict: [port: :number], aliases: [p: :port])
     [app: :slugmenu,
       version: "0.0.1",
       elixir: "~> 1.0",
-      deps: deps]
+      deps: deps,
+      port: opts[:port]
+    ]
   end
 
   # Configuration for the OTP application
