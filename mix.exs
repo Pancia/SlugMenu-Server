0e60bfd1b6b15c9ae1c0868522170820f602809b
Anthony D'Ambrosio <nsa.anthony@gmail.com>
2015-02-23 21:27:20 -0800

    Server can now parse ucsc dh info and serve a json menu!
    Using Floki for http parsing, timex for date, ets for caching the menu,
    HTTPoison to get(http) html menu.

--- mix.exs
+++ mix.exs
@@ -15,19 +15,19 @@ defmodule Slugmenu.Mixfile do
     ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
-  def application do
-    [applications: [:logger, :pipe],
-      mod: {Slugmenu, []}
-    ]
-  end
-
   defp deps do
     [{:pipe, github: "batate/elixir-pipes"},
       {:urna, github: "meh/urna"},
-      {:httpoison, "~> 0.5"}
+      {:httpoison, "~> 0.5"},
+      {:floki, "~> 0.1.0"},
+      {:timex, "~> 0.13.3"}
+    ]
+  end
+
+  # Configuration for the OTP application
+  def application do
+    [applications: [:logger, :pipe],
+      mod: {Slugmenu, []}
     ]
   end
 end
